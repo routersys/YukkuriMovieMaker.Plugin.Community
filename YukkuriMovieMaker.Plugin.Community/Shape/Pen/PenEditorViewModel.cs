19550e6f510d05bdda66fd281e0ad15926a48a39
routersys <github.routersys@gmail.com>
2026-09-07 17:57:19 +0900

    編集画面にレイヤーパネルを追加

--- YukkuriMovieMaker.Plugin.Community/Shape/Pen/PenEditorViewModel.cs
+++ YukkuriMovieMaker.Plugin.Community/Shape/Pen/PenEditorViewModel.cs
@@ -3,6 +3,7 @@ using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using YukkuriMovieMaker.Commons;
 using YukkuriMovieMaker.Player.Video;
 using FrameTime = YukkuriMovieMaker.Player.Video.FrameTime;
@@ -18,6 +19,10 @@ namespace YukkuriMovieMaker.Plugin.Community.Shape.Pen
         readonly IShapeSource documentSource;
         readonly PenPreviewRenderer previewRenderer;
         readonly TimelineItemSourceDescription documentDescription;
+        readonly Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+
+        int layerNumber;
+        bool isRenderQueued;
 
         public double CanvasWidth { get; }
 
@@ -32,16 +37,21 @@ namespace YukkuriMovieMaker.Plugin.Community.Shape.Pen
         public ImmutableList<PenLayer> Layers
         {
             get => document.Layers;
-            set
-            {
-                document.Layers = value;
-                ActiveLayer = value.IsEmpty ? null : value[^1];
-                OnPropertyChanged();
-                UpdateDocumentImage();
-            }
+            set => SetLayers(value, value.IsEmpty ? null : value[^1]);
         }
 
-        public PenLayer? ActiveLayer { get => activeLayer; set => Set(ref activeLayer, value); }
+        public ImmutableList<PenLayer> DisplayLayers { get => displayLayers; private set => Set(ref displayLayers, value); }
+        ImmutableList<PenLayer> displayLayers = [];
+
+        public PenLayer? ActiveLayer
+        {
+            get => activeLayer;
+            set
+            {
+                if (Set(ref activeLayer, value))
+                    UpdateCommands();
+            }
+        }
         PenLayer? activeLayer;
 
         public Color WetInkColor { get => wetInkColor; set => Set(ref wetInkColor, value); }
@@ -49,6 +59,16 @@ namespace YukkuriMovieMaker.Plugin.Community.Shape.Pen
 
         public double WetInkThickness { get => wetInkThickness; set => Set(ref wetInkThickness, value); }
         double wetInkThickness = PenSettings.Default.PenStyle.StrokeThickness;
+
+        public ActionCommand AddLayerCommand { get; }
+
+        public ActionCommand DuplicateLayerCommand { get; }
+
+        public ActionCommand DeleteLayerCommand { get; }
+
+        public ActionCommand MoveLayerUpCommand { get; }
+
+        public ActionCommand MoveLayerDownCommand { get; }
 
         public PenEditorViewModel(IEditorInfo info)
         {
@@ -76,8 +96,16 @@ namespace YukkuriMovieMaker.Plugin.Community.Shape.Pen
                 []);
             documentDescription = new TimelineItemSourceDescription(timelineDescription, 0, 1, 0);
 
+            AddLayerCommand = new ActionCommand(_ => true, _ => AddLayer());
+            DuplicateLayerCommand = new ActionCommand(_ => activeLayer is not null, _ => DuplicateLayer());
+            DeleteLayerCommand = new ActionCommand(_ => activeLayer is not null && document.Layers.Count > 1, _ => DeleteLayer());
+            MoveLayerUpCommand = new ActionCommand(_ => CanMoveLayer(1), _ => MoveLayer(1));
+            MoveLayerDownCommand = new ActionCommand(_ => CanMoveLayer(-1), _ => MoveLayer(-1));
+
+            document.UndoRedoCommandCreated += OnDocumentChanged;
+
             backgroundImage = BackgroundImage = RenderBackground();
-            EnsureActiveLayer();
+            AddLayer();
             UpdateDocumentImage();
         }
 
@@ -86,29 +114,125 @@ namespace YukkuriMovieMaker.Plugin.Community.Shape.Pen
             if (stylusPoints.Count == 0)
                 return;
 
-            EnsureActiveLayer();
             var layer = activeLayer;
-            if (layer is null)
+            if (layer is null || layer.IsLocked || !layer.IsVisible)
                 return;
 
             var stroke = new Stroke(stylusPoints, PenStyleFactory.CreatePen());
             layer.Strokes = layer.Strokes.Add(new SerializableStroke(stroke));
-            UpdateDocumentImage();
         }
 
-        void EnsureActiveLayer()
+        void AddLayer()
         {
-            if (activeLayer is not null && document.Layers.Contains(activeLayer))
-                return;
-            if (!document.Layers.IsEmpty)
-            {
-                ActiveLayer = document.Layers[^1];
-                return;
-            }
+            var layer = new PenLayer { Name = CreateLayerName() };
+            var layers = document.Layers;
+            var index = activeLayer is null ? layers.Count : layers.IndexOf(activeLayer) + 1;
+            SetLayers(layers.Insert(index, layer), layer);
+        }
 
-            var layer = new PenLayer();
-            document.Layers = [layer];
-            ActiveLayer = layer;
+        void DuplicateLayer()
+        {
+            var layer = activeLayer;
+            if (layer is null)
+                return;
+
+            var copy = new PenLayer
+            {
+                Name = CreateLayerName(),
+                IsVisible = layer.IsVisible,
+                IsLocked = layer.IsLocked,
+                BlendMode = layer.BlendMode,
+                IsRangeOverridden = layer.IsRangeOverridden,
+                Strokes = layer.Strokes,
+            };
+            copy.Opacity.CopyFrom(layer.Opacity);
+            copy.Length.CopyFrom(layer.Length);
+            copy.Offset.CopyFrom(layer.Offset);
+
+            var layers = document.Layers;
+            SetLayers(layers.Insert(layers.IndexOf(layer) + 1, copy), copy);
+        }
+
+        void DeleteLayer()
+        {
+            var layer = activeLayer;
+            if (layer is null)
+                return;
+
+            var layers = document.Layers;
+            var index = layers.IndexOf(layer);
+            if (index < 0 || layers.Count <= 1)
+                return;
+
+            var next = layers.RemoveAt(index);
+            SetLayers(next, next[Math.Min(index, next.Count - 1)]);
+        }
+
+        bool CanMoveLayer(int delta)
+        {
+            if (activeLayer is null)
+                return false;
+
+            var index = document.Layers.IndexOf(activeLayer);
+            var target = index + delta;
+            return index >= 0 && target >= 0 && target < document.Layers.Count;
+        }
+
+        void MoveLayer(int delta)
+        {
+            var layer = activeLayer;
+            if (layer is null)
+                return;
+
+            var layers = document.Layers;
+            var index = layers.IndexOf(layer);
+            var target = index + delta;
+            if (index < 0 || target < 0 || target >= layers.Count)
+                return;
+
+            SetLayers(layers.RemoveAt(index).Insert(target, layer), layer);
+        }
+
+        void SetLayers(ImmutableList<PenLayer> layers, PenLayer? active)
+        {
+            document.Layers = layers;
+            DisplayLayers = layers.Reverse();
+            ActiveLayer = active;
+            OnPropertyChanged(nameof(Layers));
+            UpdateCommands();
+            InvalidateDocument();
+        }
+
+        string CreateLayerName()
+        {
+            layerNumber++;
+            return string.Format(Texts.LayerDefaultName, layerNumber);
+        }
+
+        void UpdateCommands()
+        {
+            DuplicateLayerCommand.RaiseCanExecuteChanged();
+            DeleteLayerCommand.RaiseCanExecuteChanged();
+            MoveLayerUpCommand.RaiseCanExecuteChanged();
+            MoveLayerDownCommand.RaiseCanExecuteChanged();
+        }
+
+        void OnDocumentChanged(object? sender, YukkuriMovieMaker.UndoRedo.UndoRedoEventArgs e)
+        {
+            InvalidateDocument();
+        }
+
+        void InvalidateDocument()
+        {
+            if (isRenderQueued)
+                return;
+
+            isRenderQueued = true;
+            dispatcher.InvokeAsync(() =>
+            {
+                isRenderQueued = false;
+                UpdateDocumentImage();
+            }, DispatcherPriority.Render);
         }
 
         void UpdateDocumentImage()
@@ -130,6 +254,7 @@ namespace YukkuriMovieMaker.Plugin.Community.Shape.Pen
 
         public void Dispose()
         {
+            document.UndoRedoCommandCreated -= OnDocumentChanged;
             disposer.Dispose();
         }
     }
